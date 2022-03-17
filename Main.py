e46efcced1b78ec54e6a6f73ff2620d998ea3ee3
Shervin Jarrahi <shervin.jarrahi@gmail.com>
2022-03-17 12:47:32 +0330

    generating pdf is working well

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import uic, QtGui, QtWidgets
 import sys
+from PIL import Image
+from fpdf import FPDF
+from numpy import save
 
 
 class UI(QMainWindow):
@@ -11,20 +14,39 @@ class UI(QMainWindow):
         uic.loadUi("ui.ui", self)
         # Button clicking
         self.input_images_button.clicked.connect(self.input_image)
-        self.convert_button.clicked.connect(self.input_image)
-
-
+        self.convert_button.clicked.connect(self.convert)
         self.show()
         
 
     def input_image(self):
         try:
-            path = QtWidgets.QFileDialog.getOpenFileNames(None, 'Open a file', '','All Files (*.*)')
-            print(path)
+            self.input_path = QtWidgets.QFileDialog.getOpenFileNames(None, 'Open a file', '','All Files (*.*)')
+        
         except:
             image_message = QtWidgets.QMessageBox()
-            image_message.warning(self, "Choosing image", "Please choose an image", QtWidgets.QMessageBox.Ok)
-        return None
+            image_message.warning(self, "Try Again", "Something went wrong, Please try again.", QtWidgets.QMessageBox.Ok)
+
+
+    def convert(self):
+        try:
+            save_path = QtWidgets.QFileDialog.getSaveFileName(None, 'Save file', '','PDF files (*.pdf)')
+            img1 = Image.open(self.input_path[0][0])
+            img1.convert("RGB")
+            width, height = img1.size
+            pdf = FPDF(unit="pt", format=[width, height])
+            # pdf = FPDF()
+            for image in self.input_path[0]:
+                print("HI")
+                pdf.add_page()
+                pdf.image(image)
+
+            print(save_path)
+            pdf.output(save_path[0], "F")
+            # self.result_label.setText("PDF file has been saved")
+        
+        except:
+            image_message = QtWidgets.QMessageBox()
+            image_message.warning(self, "Try Again", "Something went wrong, Please try again.", QtWidgets.QMessageBox.Ok)
 
 
 if __name__ == "__main__":
